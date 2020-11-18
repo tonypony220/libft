f7ec55292e6094099deec85fdcc98ab8f98c9f41
Marq Ehtel <mehtel@vo-j3.msk.21-school.ru>
2020-11-18 20:29:41 +0300

    new

--- ft_split.c
+++ ft_split.c
@@ -12,26 +12,42 @@
 
 #include "libft.h"
 
-char		**ft_split(char const *s, char c)
+static char		*fill_str(char const *s, long int *start, long int *end)
 {
-	int		start;
-	int		end;
-	int		arr_len;
-	char	**arr;
-	char	*word;
-	int		i;
+	char		*word;
 
-	end = -1;
-	start = 0;
-	arr_len = 0;
-	while (s[++end])
-		(s[end] != c && (s[end + 1] == c || !s[end + 1])) ? \
-				   arr_len++ : 1;
-	if (!(arr = (char**)malloc(sizeof(char**) * (arr_len + 1))))
+	if ((*end - *start) && (word = \
+				(char*)malloc(sizeof(char) * (*end - *start + 1))))
+	{
+		word[*end - *start] = '\0';
+		while (*start != (*end)--)
+		{
+			word[*end - *start] = s[*end];
+		}
+		*start += ft_strlen(word) + 1;
+		return (word);
+	}
+	else
 		return (0);
+}
+
+static void		free_strs(char **arr, long int i)
+{
+	while (i)
+		free(arr[i]);
+	free(arr[i]);
+}
+
+static int		fill_arr(char **arr, long int arr_len, char const *s, char c)
+{
+	long int	start;
+	long int	end;
+	long int	i;
+	char		*word;
+
+	start = 0;
 	end = 0;
 	i = 0;
-	arr[arr_len] = 0;
 	while (i < arr_len)
 	{
 		while (s[start] == c)
@@ -39,16 +55,34 @@ char		**ft_split(char const *s, char c)
 		end = start;
 		while (!(s[end] == c) && s[end])
 			end++;
-		if ((end - start) && (word = \
-					(char*)malloc(sizeof(char) * (end - start + 1))))
+		if (!(word = fill_str(s, &start, &end)))
 		{
-			word[end - start] = '\0';
-
-			while (start != end--)
-				word[end - start] = s[end];
-			start += ft_strlen(word) + 1 ;
+			free_strs(arr, i - 1);
+			return (0);
 		}
 		arr[i++] = word;
+	}
+	return (1);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	int			end;
+	long int	arr_len;
+	char		**arr;
+
+	end = -1;
+	arr_len = 0;
+	while (s[++end])
+		(s[end] != c && (s[end + 1] == c || !s[end + 1])) ? \
+			arr_len++ : 1;
+	if (!(arr = (char**)malloc(sizeof(char**) * (arr_len + 1))))
+		return (0);
+	arr[arr_len] = 0;
+	if (!fill_arr(arr, arr_len, s, c))
+	{
+		free(arr);
+		arr = 0;
 	}
 	return (arr);
 }
